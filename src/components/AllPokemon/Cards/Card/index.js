24194ce76ed907bee068357f736597618141ad30
Fred Meziere <Fredzem@outlook.fr>
2023-06-19 22:14:01 +0200

    Feat : Ajouts de différents types ++ + modifs divers du sass

--- src/components/AllPokemon/Cards/Card/index.js
+++ src/components/AllPokemon/Cards/Card/index.js
@@ -10,17 +10,29 @@ function Card({
   function getColorForType(type) {
     switch (type) {
       case 'Grass':
-        return 'green';
+        return 'rgb(35,149,2)';
       case 'Poison':
-        return 'purple';
+        return 'rgb(158,0,255)';
       case 'Fire':
         return 'rgb(149, 27, 27)';
       case 'Water':
-        return 'blue';
+        return 'rgb(0,142,255)';
       case 'Normal':
-        return 'grey';
+        return 'rgb(153,153,153)';
       case 'Bug':
         return 'rgb(136, 255, 0)';
+      case 'Flying':
+        return 'rgb(215,215,215)';
+      case 'Fairy':
+        return 'rgb(255,0,245)';
+      case 'Electric':
+        return 'rgb(251,255,96)';
+      case 'Ground':
+        return 'rgb(129,68,46)';
+      case 'Fighting':
+        return 'rgb(87, 87, 87)';
+      case 'Psychic':
+        return 'url(/backgroundimg/psy.png';
       default:
         return 'gray'; // Couleur par défaut si le type n'est pas reconnu
     }
@@ -31,10 +43,37 @@ function Card({
       return getColorForType(types[0]);
     } if (types.length === 2) {
       if (types.includes('Grass') && types.includes('Poison')) {
-        return 'linear-gradient(315deg, rgba(158,0,255,1) 0%, rgba(35,149,2,1) 100%)';
+        return 'linear-gradient(315deg, rgba(35,149,2,1) 0%, rgba(158,0,255,1) 100%)';
       }
       if (types.includes('Fire') && types.includes('Flying')) {
-        return 'linear-gradient(315deg, rgba(177,179,176,1) 0%, rgba(184,24,57,1) 100%)';
+        return 'linear-gradient(315deg, rgba(149,27,27,1) 0%, rgba(215,215,215,1) 100%)';
+      }
+      if (types.includes('Bug') && types.includes('Flying')) {
+        return 'linear-gradient(315deg, rgba(136,255,0,1) 0%, rgba(177,179,176,1) 100%)';
+      }
+      if (types.includes('Bug') && types.includes('Poison')) {
+        return 'linear-gradient(315deg, rgba(136,255,0,1) 0%, rgba(106,0,255,1)  100%)';
+      }
+      if (types.includes('Bug') && types.includes('Fairy')) {
+        return 'linear-gradient(315deg, rgba(136,255,0,1) 0%, rgba(255,0,245,1)  100%)';
+      }
+      if (types.includes('Bug') && types.includes('Grass')) {
+        return 'linear-gradient(315deg, rgba(136,255,0,1) 0%, rgba(35,149,2,1) 100%)';
+      }
+      if (types.includes('Normal') && types.includes('Flying')) {
+        return 'linear-gradient(315deg, rgba(153,153,153,1) 0%, rgba(215,215,215,1) 100%)';
+      }
+      if (types.includes('Poison') && types.includes('Ground')) {
+        return 'linear-gradient(315deg, rgba(106,0,255,1) 0%, rgba(129,68,46,1) 100%)';
+      }
+      if (types.includes('Normal') && types.includes('Fairy')) {
+        return 'linear-gradient(315deg, rgba(153,153,153,1) 0%, rgba(255,0,245,1) 100%)';
+      }
+      if (types.includes('Poison') && types.includes('Flying')) {
+        return 'linear-gradient(315deg, rgba(158,0,255,1) 0%, rgba(215,215,215,1) 100%)';
+      }
+      if (types.includes('Water') && types.includes('Fighting')) {
+        return 'linear-gradient(315deg, rgba(0,142,255,1) 0%, rgba(87,87,87,1) 100%)';
       }
     }
     return 'gray'; // Couleur par défaut si le nombre de types est incorrect
@@ -47,7 +86,7 @@ function Card({
     <div className="card" style={{ background: backgroundColor }}>
       <img className="card-img" src={imagePath} alt={name.french} />
       <p className="card-name">{name.french}</p>
-      <p className="card-type">{type.join(', ')}</p>
+      <p className="card-type">Type: {type.join(', ')}</p>
       <div className="card-stats">
         <p className="card-hp">HP: {base.HP}</p>
         <p className="card-att">Attack: {base.Attack}</p>
