3ec3e3d7bb35a877f673b9cd8342b0a33b4b7843
Fred Meziere <Fredzem@outlook.fr>
2023-06-20 19:49:18 +0200

    Feat: Ajout d'un select pour pouvoir aussi choisir par type

--- src/components/AllPokemon/index.js
+++ src/components/AllPokemon/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './styles.scss';
 import Cards from './Cards';
 import pokeData from '../../../public/data/pokedex.json';
+import pokemonTypes from '../../../public/data/types.json';
 
 function AllPokemon() {
   const [searchText, setSearchText] = useState('');
@@ -16,25 +17,45 @@ function AllPokemon() {
   // eslint-disable-next-line no-shadow
   const filterData = (searchText) => {
     const filteredData = pokeData.filter((pokemon) => {
-      const { name, type } = pokemon;
+      const { name } = pokemon;
       const lowercaseSearchText = searchText.toLowerCase();
-      const lowercaseName = name.french.toLowerCase();
-      const lowercaseTypes = type.map((pokemonType) => pokemonType.toLowerCase());
-      return lowercaseName.includes(lowercaseSearchText) || lowercaseTypes.includes(lowercaseSearchText);
+      const lowercaseName = name.french.toLowerCase() || name.english.toLowerCase();
+      return lowercaseName.includes(lowercaseSearchText);
     });
     setResultData(filteredData);
   };
 
+  const handleTypeChange = (event) => {
+    const selectedType = event.target.value;
+    if (selectedType === '') {
+      setResultData(pokeData);
+    }
+    else {
+      const filteredData = pokeData.filter((pokemon) => pokemon.type.includes(selectedType));
+      setResultData(filteredData);
+    }
+  };
+
   return (
     <div className="allpokemon">
-      <h2 className="allpokemon-h2">La liste des pokemon</h2>
-      <div>
+      <h2 className="allpokemon-h2">La liste des Pokémon</h2>
+      <div className="allpokemon-div">
+        <p className="allpokemon-div-p">Trouve ton Pokémon plus rapidement grâce à la barre de recherche !</p>
         <input
+          className="allpokemon-div-input"
           type="text"
           value={searchText}
           onChange={handleSearchChange}
-          placeholder="Search by type or name"
+          placeholder="Écris le nom de ton Pokémon :"
         />
+        <select className="allpokemon-div-select" name="type" id="" onChange={handleTypeChange}>
+          <option value="">Tous les types</option>
+          {pokemonTypes.map((pokemonType) => (
+            <option value={pokemonType.english} key={pokemonType.english}>
+              {pokemonType.english}
+            </option>
+          ))}
+        </select>
       </div>
       <Cards datas={resultData} />
     </div>
