6588e763732767479079f6914fd2ffcd27b2bac8
Fred Meziere <Fredzem@outlook.fr>
2023-06-29 20:10:46 +0200

    Feat: Data pour pokemon supplémentaires

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
     <div className="header">
       <img className="header-img" src="../logo/pokelogo.png" alt="Logo Pokedex" />
       <h1 className="header-h1">La bible des Pokémon est ici !</h1>
-      <p className="header-p">Disclamer : Ce site est uniquement à but de montrer mes savoir-faire de développeur web. Il n'est pas à 100% fiable, des raccourcis sur les évolutions ont été effectués. Il s'agit d'un site "de travail"</p>
+      <p className="header-p">Disclamer : Ce site est uniquement à but de montrer mes savoir-faire de développeur web. Il n'est pas à 100% fiable, des raccourcis sur les évolutions ont été effectués. Il s'agit d'un site "de travail toute la data viens de https://www.pokepedia.fr/Foretress, ajoutée dans un fichier json"</p>
     </div>
   );
 }
