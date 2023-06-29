0c6e111093ad9b4f7c3e7fa3364c8169f371cf66
Fred Meziere <Fredzem@outlook.fr>
2023-06-29 15:18:13 +0200

    Feat: Data pour pokemon supplémentaires

--- src/components/AllPokemon/index.js
+++ src/components/AllPokemon/index.js
@@ -53,6 +53,7 @@ function AllPokemon() {
       <h2 className="allpokemon-h2">La liste des Pokémon</h2>
       <div className="allpokemon-div">
         <p className="allpokemon-div-p">Vous retrouverez ici l'ensemble des Pokémon de génération une à neuf, présentés sous forme de carte. Pour plus de réactivité du site, ils sont rangés neuf par neuf. Vous pouvez trier les pokemon par nom, mais aussi par type mais aussi par génération.</p>
+        <p className="allpokemon-div-p">Cliquez sur les cartes pour en découvrir un peu plus.</p>
         <p className="allpokemon-div-p">Trouve ton Pokémon plus rapidement grâce à la barre de recherche !</p>
         <input
           className="allpokemon-div-input"
